237d1dc47a20a11b32b648917b18b6478c900db9
NickCarpenter703 <carpenternick809@gmail.com>
2022-11-08 17:04:32 -0500

    Bookmark View in Profiles

---
+++ Lab/Pages/DataClasses/UserBookmark.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab.Pages.DataClasses
+{
+    public class UserBookmark
+    {
+        public int userID { get; set; }
+
+        public string firstName { get; set; }
+
+        public string secondName { get; set; }
+        
+        public string username { get; set; }
+
+        public string email { get; set; }
+
+        public string jmuType { get; set; }
+
+        public string interests { get; set; }
+
+        public string experience { get; set; }
+
+        public string gradYear { get; set; }
+
+        public string major { get; set; }
+
+        public string minor { get; set; }
+
+        public string jobTitle { get; set; }
+
+        public string department { get; set; }
+
+        public string moreInfo { get; set; }
+
+        public int bookmarkID { get; set; }
+
+        public int otherUserID { get; set; }
+
+        public string reason { get; set; }
+
+        public int profilePicID { get; set; }
+
+        public string fileName { get; set; }
+    }
+}
