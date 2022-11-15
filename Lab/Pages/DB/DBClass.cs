9fbc87c2d5fe48e38999595e19da64dc9a30b166
zainf23 <zainfarooq239@gmail.com>
2022-11-15 16:13:18 -0500

    Add AWS connection string

--- Lab/Pages/DB/DBClass.cs
+++ Lab/Pages/DB/DBClass.cs
@@ -8,10 +8,19 @@ namespace Lab.Pages.DB
     public class DBClass
     {
         private static readonly string LabConnStr
-            = @"Server=Localhost;Database=Capstone;Trusted_Connection=True";
+            = @"Server=capstone.cjzu3rtz5sjd.us-east-1.rds.amazonaws.com;
+                    Database=Capstone;uid=adminuser;password=Swoopes1";
 
         private static readonly string AuthConnStr
-           = @"Server=Localhost;Database=AUTH;Trusted_Connection=True";
+            = @"Server=capstone.cjzu3rtz5sjd.us-east-1.rds.amazonaws.com;
+                    Database=Auth;uid=adminuser;password=Swoopes1";
+
+        //private static readonly string LabConnStr
+        //    = @"Server=Localhost;Database=Capstone;Trusted_Connection=True";
+
+        //private static readonly string AuthConnStr
+        //   = @"Server=Localhost;Database=AUTH;Trusted_Connection=True";
+
         // method to read data from user file
         public static SqlDataReader UserReader(string username)
         {
