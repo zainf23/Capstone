2e217fb82e462d48a8d46c584f7d194cf5366639
zainf23 <zainfarooq239@gmail.com>
2022-12-02 22:52:43 -0500

    Removed download function

--- Lab/Pages/AWSupload/AmazonS3Uploader.cs
+++ Lab/Pages/AWSupload/AmazonS3Uploader.cs
@@ -46,41 +46,5 @@ namespace Lab.Pages.AWSupload
             }
         }
 
-        public async Task<byte[]> DownloadFileAsync(string file)
-        {
-            MemoryStream ms = null;
-
-            try
-            {
-                GetObjectRequest getObjectRequest = new GetObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = file
-                };
-
-                IAmazonS3 client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
-
-                using (var response = await client.GetObjectAsync(getObjectRequest))
-                {
-                    if (response.HttpStatusCode == HttpStatusCode.OK)
-                    {
-                        using (ms = new MemoryStream())
-                        {
-                            await response.ResponseStream.CopyToAsync(ms);
-                        }
-                    }
-                }
-
-                if (ms is null || ms.ToArray().Length < 1)
-                    throw new FileNotFoundException(string.Format("The document '{0}' is not found", file));
-
-                return ms.ToArray();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
     }
 }
