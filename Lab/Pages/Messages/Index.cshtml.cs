ccf55c8569b06a35ab6a7065b22e081677e067a2
farquand000 <farquand000@gmail.com>
2022-12-02 20:20:52 -0500

    changes

--- Lab/Pages/Messages/Index.cshtml.cs
+++ Lab/Pages/Messages/Index.cshtml.cs
@@ -64,6 +64,11 @@ namespace Lab.Pages.Messages
             }
             messageReader.Close();
 
+            if (MyMessages.Count == 0)
+            {
+                ViewData["ErrorMessage"] = "You have no Messages!";
+            }
+
 
             string sqlQuery1 = "Select distinct m.messageID, m.userID, m.otherUserID, m.subject, m.message, m.readMessage, m.date, m.senderName, upp.fileName from Messages m, UserProfilePic upp WHERE readMessage = 1 AND m.userID = upp.userID AND m.otherUserID = " + UserData.userID + "Order by messageID desc";
 
@@ -86,6 +91,7 @@ namespace Lab.Pages.Messages
                 });
             }
             readReader.Close();
+
         }
     }
 }
